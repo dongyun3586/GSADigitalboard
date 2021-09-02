ad84365eeaf88ed278be9887a03d9785c0e51780
leedong <goodluck3586@gmail.com>
2021-09-02 20:35:29 +0900

    article list modify and delete
    게시물 목록 페이지 수정, 삭제 기능 구현

--- models/articleDAO.js
+++ models/articleDAO.js
@@ -1,5 +1,6 @@
 var connection = require('./db')
 
+// 이미지 게시물
 exports.selectShowImage = () => new Promise((resolve, reject)=>{
     const query = 'SELECT * FROM article WHERE date_start_post<=date(now()) and date_end_post>=date(now()) and type=?';
     connection.query(query, 'image', function (error, results, fields){
@@ -10,6 +11,7 @@ exports.selectShowImage = () => new Promise((resolve, reject)=>{
     })
 })
 
+// 동영상 게시물
 exports.selectShowVideo = () => new Promise((resolve, reject)=>{
     const query = 'SELECT * FROM article WHERE date_start_post<=date(now()) and date_end_post>=date(now()) and type=?';
     connection.query(query, 'video', function (error, results, fields){
@@ -20,6 +22,7 @@ exports.selectShowVideo = () => new Promise((resolve, reject)=>{
     })
 })
 
+// 게시물 추가
 exports.insertArticle = (body, placeSum, imgFilename, writerEmail, fileType)=> new Promise((resolve, reject)=>{
     let query = `INSERT INTO article(title, date_start_post, date_end_post, place, file_path, writer_email, message, type) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`;
     connection.query(query, [body.title, body.date_start_post, body.date_end_post, placeSum, imgFilename, writerEmail, body.message, fileType],
@@ -32,6 +35,7 @@ exports.insertArticle = (body, placeSum, imgFilename, writerEmail, fileType)=> n
     });
 })
 
+// 전체 게시물 목록(최신순)
 exports.selectArticles=()=>new Promise((resolve, reject)=>{
     let query = 'SELECT * FROM article ORDER BY idarticle DESC';
     connection.query(query, (error, results, fields)=>{
@@ -42,6 +46,54 @@ exports.selectArticles=()=>new Promise((resolve, reject)=>{
         }
     })
 })
+
+// idarticle로 게시물 조회
+exports.selectArticleById = (id)=>new Promise((resolve, reject)=>{
+    let sql = 'SELECT * FROM article WHERE idarticle = ?'
+    connection.query(sql, [id], (error, results, fields)=>{
+        if(error){
+            reject(error);
+        }else{
+            resolve(results);
+        }
+    })
+})
+
+// idarticle로 게시물 삭제
+exports.deleteArticleById = (idarticle)=>new Promise((resolve, reject)=>{
+    sql = 'DELETE FROM article WHERE idarticle = ?'
+    connection.query(sql, [idarticle], function(error, results, fields){
+        if(error){
+            reject(error);
+        }else{
+            resolve();
+        }
+    })
+})
+
+// idarticle로 게시물 업데이트
+exports.updateArticleById = (body, placeSum, writerEmail, idarticle)=>new Promise((resolve, reject)=>{
+    let query = 'UPDATE article SET title = ?, date_start_post = ?, date_end_post = ?, place = ?, writer_email = ?, message = ? WHERE idarticle = ?'
+    connection.query(query, [body.title, body.date_start_post, body.date_end_post, placeSum, writerEmail, body.message, idarticle], 
+        function(error, results, fields){
+        if(error){
+            reject(error);
+        }else{
+            resolve();
+        }
+    })
+})
+
+// exports.deleteArticle = (id, cb)=>{
+//     sql = `DELETE FROM article WHERE idarticle = ${id}`;
+//     connection.query(sql, function(error, results, fields){
+//         if(error){
+//             console.log('DELETE ERROR');
+//         }else{
+//             cb();
+//         }
+//     })
+// }
 
 
 // exports.selectAllArticlesCount = (cb)=>{
@@ -80,13 +132,3 @@ exports.selectArticles=()=>new Promise((resolve, reject)=>{
 
 
 
-// exports.deleteArticle = (id, cb)=>{
-//     sql = `DELETE FROM article WHERE idarticle = ${id}`;
-//     connection.query(sql, function(error, results, fields){
-//         if(error){
-//             console.log('DELETE ERROR');
-//         }else{
-//             cb();
-//         }
-//     })
-// }
--- routes/notice.js
+++ routes/notice.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../models/articleDAO');
-var fs = require('fs')
+const fs = require('fs/promises');
 const multer = require('multer');
 const { GatewayTimeout } = require('http-errors');
 
@@ -39,35 +39,43 @@ var upload = multer({ storage: storage })
 // 이미지 게시물 요청
 router.get('/show_image', async (req, res, next)=>{
   let results = await model.selectShowImage();
-  //console.log('/show_image results : ', results)
-  res.render('show_image', {results: results})
+  if(results.length===0){
+    res.send('<h1>게시물이 없습니다!!! <a href="/">Home</a><h1>')
+  }else{
+    //console.log('/show_image results : ', results)
+    res.render('show_image', {results: results})
+  }
 });
 
 // 동영상 게시물 요청
 router.get('/show_video/:id', async (req, res, next)=>{
   let results = await model.selectShowVideo();
-  console.log('/show_video results : ', results)
-  // 현재 비디오와 다음 비디오
-  videoList=[]
-  let currentVideo;
-  let nextVideo;
-  if(results!=undefined){
-    for(var i=0; i<results.length; i++){
-      videoList.push(results[i].file_path)
+  if(results.length===0){
+    res.redirect('/notice/show_image')
+  }else{
+    //console.log('/show_video results : ', results)
+    // 현재 비디오와 다음 비디오
+    videoList=[]
+    let currentVideo;
+    let nextVideo;
+    if(results!=undefined){
+      for(var i=0; i<results.length; i++){
+        videoList.push(results[i].file_path)
+      }
+      if(req.params.id == results.length-1){
+        currentVideo = `/uploadFiles/videos/${videoList[req.params.id]}`   
+        nextVideo = 'http://localhost:3000/notice/show_image'        
+      }else{
+        currentVideo = `/uploadFiles/videos/${videoList[req.params.id]}`   
+        nextVideo = `http://localhost:3000/notice/show_video/${parseInt(req.params.id)+1}` 
+      }
     }
-    if(req.params.id == results.length-1){
-      currentVideo = `/uploadFiles/videos/${videoList[req.params.id]}`   
-      nextVideo = 'http://localhost:3000/notice/show_image'        
-    }else{
-      currentVideo = `/uploadFiles/videos/${videoList[req.params.id]}`   
-      nextVideo = `http://localhost:3000/notice/show_video/${parseInt(req.params.id)+1}` 
-    }
+    console.log('videoList', videoList)   // videoList [ '2021-8-30-mov_bbb.mp4', '2021-8-30-movie.mp4' ]
+    // console.log('videoList[req.params.id]', videoList[req.params.id])
+    console.log('currentVideo', currentVideo);
+    console.log('nextVideo', nextVideo)
+    res.render('show_video', {currentVideo: currentVideo, nextVideo: nextVideo})
   }
-  console.log('videoList', videoList)   // videoList [ '2021-8-30-mov_bbb.mp4', '2021-8-30-movie.mp4' ]
-  // console.log('videoList[req.params.id]', videoList[req.params.id])
-  console.log('currentVideo', currentVideo);
-  console.log('nextVideo', nextVideo)
-  res.render('show_video', {currentVideo: currentVideo, nextVideo: nextVideo})
 })
 
 // 새로운 게시물 작성 화면 요청
@@ -82,16 +90,7 @@ router.get('/write', function(req, res) {
 router.post('/write', upload.single('file'), async (req, res)=>{
   console.log('req.body', req.body);
 
-  // 장소 체크에 따른 숫자 저장 (급식실 = 1, 기숙사 = 2, 학교홍보 = 4)
-  let placeSum = 0;
-  if(Array.isArray(req.body.place)){
-    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
-    placeSum = req.body.place.reduce(reducer);
-  }else{
-    placeSum = parseInt(req.body.place); 
-  }
-  console.log('placeSum', placeSum);
-  
+  let placeSum = SumCheckPlace(req.body.place);
   let imgFileName = req.file.path.substring(26);
   console.log('req.file.path', req.file.path.substring(26)) // 파일명만 남김
 
@@ -111,12 +110,67 @@ router.get('/list', async (req, res)=>{
   res.render('show_notice_list', {results: results, isLogin: true, userEmail: 'ldy@gsa.hs.kr'})
 })
 
-// 업로드된 이미지 목록에서 이미지 정보 수정
+// 게시물 삭제
+router.get('/delete/:id', async (req, res)=>{
+  const article = await model.selectArticleById(req.params.id);
+  console.log('article : ', article);
+  console.log(article[0].file_path)
+  // public 폴더에서 파일 제거
+  let removeFilePath='';
+  if(article[0].type==='image'){
+    removeFilePath=`./public/uploadFiles/images/${article[0].file_path}`;
+  }else if(article[0].type==='video'){
+    removeFilePath=`./public/uploadFiles/videos/${article[0].file_path}`;
+  }
+  try {
+    await fs.unlink(removeFilePath);
+    console.log(`successfully deleted ${removeFilePath}`);
+    // DB에서 목록 제거
+    await model.deleteArticleById(req.params.id)
+  } catch (error) {
+    console.error('there was an error:', error.message);
+  }
+  res.redirect('/notice/list')
+})
+
+// 게시물 수정 화면 요청
+router.get('/modify/:id', async (req, res)=>{
+  const results = await model.selectArticleById(req.params.id);
+  let today = new Date(); 
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1;  // 월
+  let date = today.getDate();  // 날짜
+  console.log(`${year}-${month}-${date}`)
+  res.render('modify_notice', {results: results[0], today: `${year}-${month}-${date}`})
+})
 
 
-// 업로드된 이미지 목록에서 이미지 삭제
+// 게시물 수청 요청 처리
+router.post('/modify/:id', async (req, res)=>{
+  // 장소 체크에 따른 숫자 저장 (급식실 = 1, 기숙사 = 2, 학교홍보 = 4)
+  let placeSum = SumCheckPlace(req.body.place);
+  
+  // db에 저장
+  try {
+    await model.updateArticleById(req.body, placeSum, 'ldy@gsa.hs.kr', req.params.id);
+  } catch (error) {
+    console.log(error);
+  }
+  //res.send(req.body);
+  res.redirect('/notice/list');  // 이미지 목록을 보여주는 화면으로 전환되도록 한다.
+})
 
-
+// 장소 체크에 따른 숫자 저장 (급식실 = 1, 기숙사 = 2, 학교홍보 = 4)
+function SumCheckPlace(place){
+  let placeSum = 0;
+  if(Array.isArray(place)){
+    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+    placeSum = place.reduce(reducer);
+  }else{
+    placeSum = parseInt(place); 
+  }
+  return placeSum
+}
 
 
 
